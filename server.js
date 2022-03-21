063e923734423f38871c12f64ef80b1c8c3429e2
dogmom3 <blair.seivers@yahoo.com>
2022-03-21 10:41:27 -0400

    update port

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.get('/api/animals', (req, res) => {
   }
   res.json(results);
 });
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log(`API server now on port 3001!`);
 });
